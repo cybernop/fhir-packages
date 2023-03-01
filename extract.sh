ffdc24b11548ef3ceb76e702ff7afbc69358321a
Alexander Schulze <cibernop@mable-anex.de>
2023-03-01 15:21:43 +0100

    update extratc script to take output dir as argument

--- extract.sh
+++ extract.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+targetdir="$1"
+mkdir -p $targetdir
+
 SCRIPT_DIR="$(dirname $0)"
 for f in $( find "$SCRIPT_DIR" -type f -name '*.tgz' )
 do
     filename=$(basename $f .tgz)
     dirname=$(echo "$filename" | sed -r "s/([a-z\.]+)-([a-z0-9\.-]+)/\1#\2/")
-    outputdir="$dirname/"
+    outputdir="$targetdir/$dirname/"
     echo "extract $dirname"
     mkdir -p $outputdir
     tar -xzf $f -C $outputdir
