76ce816fc7e4fc9229fcf300c0a5bbef63872fb6
Alexander Schulze <cibernop@mable-anex.de>
2023-03-13 16:53:24 +0100

    fix issues with numbers in package name

--- extract.sh
+++ extract.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(dirname $0)"
 for f in $( find "$SCRIPT_DIR" -type f -name '*.tgz' )
 do
     filename=$(basename $f .tgz)
-    dirname=$(echo "$filename" | sed -r "s/([a-z\.]+)-([a-z0-9\.-]+)/\1#\2/")
+    dirname=$(echo "$filename" | sed -r "s/([a-z0-9\.]+)-([a-z0-9\.-]+)/\1#\2/")
     outputdir="$targetdir/$dirname/"
     echo "extract $dirname"
     mkdir -p $outputdir
